eb611ea7d8fe39fc594c978524201154ec2aacbf
Ihor Shynkar <aedricson@gmail.com>
2025-02-07 20:21:51 +0200

    added readme

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/car-dealer-app",
+  output: "export", 
+  reactStrictMode: true,
 };
 
 export default nextConfig;
